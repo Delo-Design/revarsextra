3eccba2dd8b84d2de384954f0db291a43adf2df7
progreccor <boss@delo-design.ru>
2022-02-01 19:56:52 +0300

    1.0

--- revarsextra.php
+++ revarsextra.php
@@ -14,44 +14,44 @@ use Joomla\CMS\Plugin\CMSPlugin;
 
 
 /**
- * Variablesubdomain plugin.
+ * RevarsExtra plugin.
  *
  * @package   revars
  * @since     1.0
  */
-class plgRevarsExtra extends CMSPlugin
+class plgRevarsRevarsextra extends CMSPlugin
 {
 
-	/**
-	 * Application object
-	 *
-	 * @var    CMSApplication
-	 * @since  1.0.0
-	 */
-	protected $app;
+    /**
+     * Application object
+     *
+     * @var    CMSApplication
+     * @since  1.0.0
+     */
+    protected $app;
 
 
-	/**
-	 * Affects constructor behavior. If true, language files will be loaded automatically.
-	 *
-	 * @var    boolean
-	 * @since  1.0.0
-	 */
-	protected $autoloadLanguage = true;
+    /**
+     * Affects constructor behavior. If true, language files will be loaded automatically.
+     *
+     * @var    boolean
+     * @since  1.0.0
+     */
+    protected $autoloadLanguage = true;
 
 
-	public function onRevarsAddVariables()
-	{
-		return [
-			(object) [
-				'variable' => '{VAR_YEAR}',
-				'value'    => date ( 'Y' )
-			],
-            [
+    public function onRevarsAddVariables()
+    {
+        return [
+            (object) [
+                'variable' => '{VAR_YEAR}',
+                'value'    => date ( 'Y' )
+            ],
+            (object) [
                 'variable' => '{VAR_MONTH}',
                 'value'    => date ( 'm' )
             ]
-		];
-	}
+        ];
+    }
 
 }
